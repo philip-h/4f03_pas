76c2e74ba61af6407b49f77b9c1a961810ccb960
philip-h <philip.habib6347@gmail.com>
2017-01-29 20:22:42 -0500

    Got enforcing f0 to work for current makefile

--- pa1.c
+++ pa1.c
@@ -16,9 +16,10 @@
 using namespace std;
 
 /* Prototypes */
-void *createString(void *);
 int checkArgs(int);
+void *createString(void *);
 void checkConditionNum(int);
+void enforce0(long);
 
 /* Global vars */
 string S = "";
@@ -84,7 +85,7 @@ int main(int argc, char* argv[]) {
     pthread_join(threads[i], NULL);
   }
 
-  printf("%s\n", S.c_str());
+  printf("%s %s %s\n", S.substr(0,6).c_str(), S.substr(6,6).c_str(), S.substr(12,6).c_str());
   printf("%d\n", numVerified);
   free(threads);
   free(c);
@@ -144,7 +145,10 @@ void *createString(void *r) {
     pthread_mutex_lock(&stringMutex);
     int lenS = S.length();
     if (lenS < m * l) {
-      S += sigma[rank];
+      if (lenS == 0)
+        S += sigma[rank];
+      else
+        enforce0(rank);
     } else {
       pthread_mutex_unlock(&stringMutex);
       checkConditionNum(rank);
@@ -158,12 +162,8 @@ void checkConditionNum(int threadRank)
 {
   if(threadRank + 1 <= m)
   {
-    int c0 = 0;
-    int c1 = 0;
-    int c2 = 0;
+    int c0 = 0, c1 = 0, c2 = 0;
 
-    //TODO: Count occurances of vars
-    int numSegments = m/n;
     for (size_t t = threadRank; t < m; t+=n) {
       printf("%d : Checking threads: %d, %d\n", threadRank, t*l, (l*t+l));
       for (size_t i = t*l; i < t*l+l; i++) {
@@ -211,4 +211,82 @@ void checkConditionNum(int threadRank)
       }
     }
   }
+}
+
+void enforce0(long threadRank) {
+
+  printf("Enforcing rule %d for thread %d\n",f, threadRank);
+  printf("Current state of S: %s\n", S.c_str());
+
+
+  // Count how many occurences of c0, c1 and c2 are already in S
+  int c0 = 0, c1 = 0, c2 = 0;
+
+  int lenS = S.length();
+  int i_start = 0, i_end = 0;
+  int numSegments = m/n;
+  // for (size_t i = 0; i < numSegments; i++) {
+  //   if (lenS < i*l + l) {
+  //     i_start = i;
+  //     i_end = i + l;
+  //     break;
+  //   }
+  // }
+
+  // HARD CODE FOR 3 SEGS of LEN 6
+  // 0 to 6
+  if (lenS < 7) {
+    i_start = 0;
+    i_end = 6;
+  } else if (lenS < 13) {
+    i_start = 6;
+    i_end = 12;
+  } else if (lenS < 19) {
+    i_start = 12;
+    i_end = 18;
+  }
+
+  printf("Checking letters %d to %d\n", i_start, i_end);
+  for (size_t i = i_start; i < i_end; i++) {
+    if(S[i] == c[0])  c0++;
+    else if(S[i] == c[1])  c1++;
+    else if(S[i] == c[2])  c2++;
+  }
+
+  printf("%d\toccurences: c0: %d, c1: %d, c2: %d \n", threadRank, c0, c1, c2);
+
+  // Calculate which letters are needed to enforce rule 1
+  int numC0Needed = (c2 - c1) - c0;
+  int numC1Needed = (c2 - c0) - c1;
+  int numC2Needed = (c0 + c1) - c2;
+
+  if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+    S += sigma[threadRank];
+  } else {
+
+  // printf("Does %c == %c\n", sigma[threadRank], c[0]);
+  // If we need some c0, add it to the string
+  if (sigma[threadRank] == c[0] ) {
+    if (numC0Needed > 0) {
+      S += sigma[threadRank];
+    }
+  }
+
+  // If we need some c1, add it to the string
+  else if (sigma[threadRank] == c[1]) {
+    if (numC1Needed > 0) {
+      S += sigma[threadRank];
+    }
+  }
+
+  // If we need some c2, add it to the string
+  else if (sigma[threadRank] == c[2]) {
+    if (numC2Needed > 0) {
+      S += sigma[threadRank];
+    }
+  }
+}
+
+
+
 }
