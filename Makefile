2cab6766630bf362e8cf62a5af346859a8bd60ba
philip-h <philip.habib6347@gmail.com>
2017-01-29 22:08:39 -0500

    Cleand the code

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CFLAGS=-lpthread -std=c++11
 
 pa1.x : pa1.c
-	g++ -o pa1.x pa1.c $(CFLAGS)
+	g++ -o $@ $^ $(CFLAGS)
 
 run: pa1.x
-	./pa1.x 2 3 3 3 a b c
+	./$^ 0 3 6 3 a b c
 
 clean:
 	rm pa1.x
--- pa1.c
+++ pa1.c
@@ -1,12 +1,13 @@
 /*
-  Contributors: Philip Habib
-                Theo Stone
+  Contributors: Philip Habib (habibp)
+                Theo Stone (stonet)
 */
 
 #include <iostream>
 #include <string>
 
 #include <cstdlib>
+#include <cmath>
 #include <string.h>
 #include <time.h>
 #include <pthread.h>
@@ -42,29 +43,24 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
-  /* Type checking the first 4 arguments*/
-
+  // Assign f,n,l,m from the command line
   f = atoi(argv[1]);
   n = atoi(argv[2]);
 
   l = atoi(argv[3]);
   m = atoi(argv[4]);
 
-  int cSize = argc-5;
-
-  c = (char*) malloc(sizeof(char)*cSize);
-
-  for (short i = 0; i < cSize; i++) {
+  // Assign which characters will be used to enforce segment rules
+  c = (char*) malloc(sizeof(char)*3);
+  for (short i = 0; i < 3; i++) {
     c[i] = *argv[i+5];
   }
 
+  // Init our alphabet from a- [c-g]
   sigma = (char*) malloc(sizeof(char)*n);
-
   for (size_t i = 0; i < n; i++) {
     sigma[i] = (char)(i + (int)'a');
   }
-
-  int len = argc - 5;
 
   if (checkArgs(argc) != 0) {
     return -1;
@@ -76,7 +72,7 @@ int main(int argc, char* argv[]) {
   srand(time(NULL));
   pthread_t* threads = (pthread_t*) malloc(n*sizeof(pthread_t));
 
-
+  printf("Starting Threads ...\n");
   for (size_t i = 0; i < n; i++) {
     pthread_create(&threads[i], NULL, createString, (void *)i);
   }
@@ -87,6 +83,8 @@ int main(int argc, char* argv[]) {
 
   printf("%s\n", S.c_str());
   printf("%d\n", numVerified);
+
+  // Free allocated memory!
   free(threads);
   free(c);
   free(sigma);
@@ -94,6 +92,10 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
+/**
+  Checks to make sure command line arguments don't break the program!
+  Will print and return with an error if there is a problem with the arguments.
+*/
 int checkArgs(int argc){
   if (f < 0 || f > 3) {
     printf("F must be between 0 and 3\n");
@@ -124,6 +126,10 @@ int checkArgs(int argc){
   return 0;
 }
 
+/**
+  Uses Threads to create a string that will adhere to some rules specified
+  in the command line arguments
+*/
 void *createString(void *r) {
   long rank = (long) r;
 
@@ -158,6 +164,10 @@ void *createString(void *r) {
   }
 }
 
+/**
+  Checks whether a certain rule is satisfied in a segment.
+  Each thread will check 1 or more segments.
+*/
 void checkConditionNum(int threadRank)
 {
   if(threadRank + 1 <= m)
@@ -165,7 +175,6 @@ void checkConditionNum(int threadRank)
     int c0 = 0, c1 = 0, c2 = 0;
 
     for (size_t t = threadRank; t < m; t+=n) {
-      printf("%d : Checking threads: %d, %d\n", threadRank, t*l, (l*t+l));
       for (size_t i = t*l; i < t*l+l; i++) {
         if(S[i] == c[0])  c0++;
         else if(S[i] == c[1])  c1++;
@@ -213,12 +222,10 @@ void checkConditionNum(int threadRank)
   }
 }
 
+/**
+  Forces the rules f0 - f1 according to arguments specified.
+*/
 void enforceRules(long threadRank) {
-
-  printf("Enforcing rule %d for thread %d\n",f, threadRank);
-  printf("Current state of S: %s\n", S.c_str());
-
-
   // Count how many occurences of c0, c1 and c2 are already in S
   int c0 = 0, c1 = 0, c2 = 0;
 
@@ -236,26 +243,27 @@ void enforceRules(long threadRank) {
     }
   }
 
-  printf("Checking letters %d to %d\n", i_start, i_end);
   for (size_t i = i_start; i < i_end; i++) {
     if(S[i] == c[0])  c0++;
     else if(S[i] == c[1])  c1++;
     else if(S[i] == c[2])  c2++;
   }
 
-  printf("%d\toccurences: c0: %d, c1: %d, c2: %d \n", threadRank, c0, c1, c2);
-
   int numC0Needed = 0, numC1Needed = 0, numC2Needed = 0;
-  // Calculate which letters are needed to enforce rule 1
+  // Calculate which letters are needed to enforce rule 0
   if (f == 0) {
     numC0Needed = (c2 - c1) - c0;
     numC1Needed = (c2 - c0) - c1;
     numC2Needed = (c0 + c1) - c2;
-  } else if (f == 1) {
+  }
+  // Calculate which letters are needed to enforce rule 1
+  else if (f == 1) {
     numC0Needed = (c2 - 2*c1) - c0;
     numC1Needed = (c2 - c0)/2 - c1;
     numC2Needed = (c0 + 2*c1) - c2;
-  } else if (f == 2) {
+    }
+    // Calculate which letters are needed to enforce rule 2
+    else if (f == 2) {
     if (c0 == 0 ) {
       numC0Needed = 1;
     } else if (c1 == 0) {
@@ -265,12 +273,15 @@ void enforceRules(long threadRank) {
       numC1Needed = (c2 / c0) - c1;
       numC2Needed = (c0 * c1) - c2;
     }
-  } else if (f == 3) {
+  }
+  // Calculate which letters are needed to enforce rule 3
+  else if (f == 3) {
     numC0Needed = (c2 + c1) - c0;
     numC1Needed = (-c2 + c0) - c1;
     numC2Needed = (c0 - c1) - c2;
   }
 
+  // if no letters are needed, any letter may be appended to S
   if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
     S += sigma[threadRank];
   }
