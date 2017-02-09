2c4b4eeabde3a57f66f68d14befcbb4a775ad363
Philip Habib <philip.habib6347@gmail.com>
2017-02-09 18:16:44 -0500

    Almost got it working, so close

--- Makefile
+++ Makefile
@@ -4,9 +4,13 @@ pa2.x : pa2.c
 	g++ -o $@ $^ $(CFLAGS)
 
 run: pa2.x
-	./$^ 0 3 6 3 a b c
+	./$^ 1 3 3 3 a b c
+	./$^ 1 3 5 6 a b c
+	./$^ 1 3 4 9 a b c
+	./$^ 1 3 7 3 a b c
 
 clean:
 	rm pa2.x
 
+    
 
--- pa2.c
+++ pa2.c
@@ -20,6 +20,11 @@ using namespace std;
 int checkArgs(int);
 void *createString(void *);
 void checkConditionNum(int);
+void enforce0(long);
+void enforce1(long);
+void enforce2(long);
+void enforce3(long);
+//TODO: delete
 void enforceRules(long);
 
 /* Global vars */
@@ -151,10 +156,12 @@ void *createString(void *r) {
     pthread_mutex_lock(&stringMutex);
     int lenS = S.length();
     if (lenS < m * l) {
-      if (lenS == 0)
-        S += sigma[rank];
-      else
-        enforceRules(rank);
+        switch(f) {
+            case 0: enforce0(rank); break;
+            case 1: enforce1(rank); break;
+            case 2: enforce2(rank); break;
+            case 3: enforce3(rank); break;
+        }
     } else {
       pthread_mutex_unlock(&stringMutex);
       checkConditionNum(rank);
@@ -175,52 +182,212 @@ void checkConditionNum(int threadRank)
     int c0 = 0, c1 = 0, c2 = 0;
 
     for (size_t t = threadRank; t < m; t+=n) {
+
       for (size_t i = t*l; i < t*l+l; i++) {
         if(S[i] == c[0])  c0++;
         else if(S[i] == c[1])  c1++;
         else if(S[i] == c[2])  c2++;
       }
-    }
+    
+        if(f == 0)
+        {
+          if((c0 + c1) == c2)
+          {
+            pthread_mutex_lock(&countMutex);
+            numVerified++;
+            pthread_mutex_unlock(&countMutex);
+          }
+        }
+        else if(f == 1)
+        {
+          if((c0 + 2*c1) == c2)
+          {
+            pthread_mutex_lock(&countMutex);
+            numVerified++;
+            pthread_mutex_unlock(&countMutex);
+          }
+        }
+        else if(f == 2)
+        {
+          if((c0 * c1) == c2)
+          {
+            pthread_mutex_lock(&countMutex);
+            numVerified++;
+            pthread_mutex_unlock(&countMutex);
+          }
+        }
+        else if(f == 3)
+        {
+          if((c0 - c1) == c2)
+          {
+            pthread_mutex_lock(&countMutex);
+            numVerified++;
+            pthread_mutex_unlock(&countMutex);
+          }
+        }
 
-
-    if(f == 0)
-    {
-      if((c0 + c1) == c2)
-      {
-        pthread_mutex_lock(&countMutex);
-        numVerified++;
-        pthread_mutex_unlock(&countMutex);
-      }
-    }
-    else if(f == 1)
-    {
-      if((c0 + 2*c1) == c2)
-      {
-        pthread_mutex_lock(&countMutex);
-        numVerified++;
-        pthread_mutex_unlock(&countMutex);
-      }
-    }
-    else if(f == 2)
-    {
-      if((c0 * c1) == c2)
-      {
-        pthread_mutex_lock(&countMutex);
-        numVerified++;
-        pthread_mutex_unlock(&countMutex);
-      }
-    }
-    else if(f == 3)
-    {
-      if((c0 - c1) == c2)
-      {
-        pthread_mutex_lock(&countMutex);
-        numVerified++;
-        pthread_mutex_unlock(&countMutex);
-      }
+        c0 = 0;
+        c1 = 0;
+        c2 = 0;
     }
   }
 }
+
+void enforce0(long rank) {
+  // Count how many occurences of c0, c1 and c2 are already in S
+  int c0 = 0, c1 = 0, c2 = 0;
+
+  int lenS = S.length();
+  int i_start = 0, i_end = 0;
+
+  // For enforcing, to check the number of letters, every thread must count the
+  // number of letters in the segment currently being written to... This for
+  // loop calculates this range!
+  for (size_t i = 0; i <= m; i++) {
+    if (lenS < l*i) {
+      i_start = l*i - l;
+      i_end = l * i;
+      break;
+    }
+  }
+
+  // Count the number of occurrences of c[0..2] in the current segment
+  for (size_t i = i_start; i < i_end; i++) {
+    if(S[i] == c[0])  c0++;
+    else if(S[i] == c[1])  c1++;
+    else if(S[i] == c[2])  c2++;
+  }
+
+  int numC0Needed = (c2 - c1) - c0;
+  int numC1Needed = (c2 - c0) - c1;
+  int numC2Needed = (c0 + c1) - c2;
+
+  if (n == 3 && l % 2 == 1) {
+      /*ERROR*/
+      fprintf(stderr, "n and l don't work for enforcing rule 0\n");
+  }
+
+
+  int lenCurrSegment = lenS % l;
+  // If the current segment is empty
+  if (lenCurrSegment == 0) {
+    // Only let in a letter used in the property check
+    if (sigma[rank] == c[0] || sigma[rank] == c[1] || sigma[rank] == c[2]) {
+        S += sigma[rank];
+    }  
+        
+  } else if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+      // If the current segment is one away from being empty
+      if (l - lenCurrSegment == 1) {
+          // And we are not appending a letter used in the property check
+          if (sigma[rank] != c[0] && sigma[rank] != c[1] && sigma[rank] != c[2]) {
+              S += sigma[rank];
+          } 
+        // If the current segment is being filled, fill it  
+      } else {
+          S += sigma[rank];
+      }
+  } else {
+      // If we are trying to add c0, and we need c0, add c0
+      if (sigma[rank] == c[0] && numC0Needed > 0) {
+          S += sigma[rank];
+      } else if (sigma[rank] == c[1] && numC1Needed > 0) {
+          S += sigma[rank];
+      } else if (sigma[rank] == c[2] && numC2Needed > 0) {
+          S += sigma[rank];
+      }
+  }
+}
+
+void enforce1(long rank) {
+   // Count how many occurences of c0, c1 and c2 are already in S
+  int c0 = 0, c1 = 0, c2 = 0;
+
+  int lenS = S.length();
+  int i_start = 0, i_end = 0;
+
+  // For enforcing, to check the number of letters, every thread must count the
+  // number of letters in the segment currently being written to... This for
+  // loop calculates this range!
+  for (size_t i = 0; i <= m; i++) {
+    if (lenS < l*i) {
+      i_start = l*i - l;
+      i_end = l * i;
+      break;
+    }
+  }
+
+  // Count the number of occurrences of c[0..2] in the current segment
+  for (size_t i = i_start; i < i_end; i++) {
+    if(S[i] == c[0])  c0++;
+    else if(S[i] == c[1])  c1++;
+    else if(S[i] == c[2])  c2++;
+  }
+
+  int numC0Needed = (c2 - 2*c1) - c0;
+  int numC1Needed = (c2 - c0)/2 - c1;
+  int numC2Needed = (c0 + 2*c1) - c2;
+
+  if (n == 3 && l == 1) {
+      /*ERROR*/
+      fprintf(stderr, "n and l don't work for enforcing rule 0\n");
+  }
+
+
+  int lenCurrSegment = lenS % l;
+  // If the current segment is empty
+  if (lenCurrSegment == 0) {
+    // Edge cases
+    if (l == 2 || l == 4) {/* Don't add a c2*/ }
+    else if (l == 3) {/* Don't add a c0 */}
+    // Only let in a letter used in the property check
+    if (sigma[rank] == c[0] || sigma[rank] == c[1] || sigma[rank] == c[2]) {
+        if (l == 2 || l == 4) {
+            /* Don't add a c2*/
+           if (sigma[rank] != c[2]) {
+               S += sigma[rank];
+           } 
+        }
+        else if (l == 3) {
+            /* Don't add a c0 */
+           if (sigma[rank] != c[0]) {
+               S += sigma[rank];
+           }
+        } else {
+            S += sigma[rank];
+        }
+    }  
+        
+  } else if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+      // 
+      if (sigma[rank] == c[0] || sigma[rank] == c[2]) {
+          // dont have enough room (REMAKE THIS COMMENT)
+          if (l - lenCurrSegment >= 2) {
+              S += sigma[rank];
+          }
+        // If the current segment is being filled, fill it  
+      } else if(sigma[rank] == c[1]){
+          if (l - lenCurrSegment >= 3) {
+              S += sigma[rank];
+          }
+      } else {
+          S += sigma[rank];
+      }
+  } else {
+      // If we are trying to add c0, and we need c0, add c0
+      if (sigma[rank] == c[0] && numC0Needed > 0) {
+          S += sigma[rank];
+      } else if (sigma[rank] == c[1] && numC1Needed > 0) {
+          S += sigma[rank];
+      } else if (sigma[rank] == c[2] && numC2Needed > 0) {
+          S += sigma[rank];
+      }
+  }   
+}
+
+
+void enforce2(long rank) {}
+void enforce3(long rank) {}
 
 /**
   Forces the rules f0 - f1 according to arguments specified.
@@ -243,6 +410,7 @@ void enforceRules(long threadRank) {
     }
   }
 
+  // Count the number of occurrences of c[0..2] in the current segment
   for (size_t i = i_start; i < i_end; i++) {
     if(S[i] == c[0])  c0++;
     else if(S[i] == c[1])  c1++;
@@ -283,6 +451,12 @@ void enforceRules(long threadRank) {
 
   // if no letters are needed, any letter may be appended to S
   if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+      // If we are beginning a new segment, only let c0, c1 or c2
+      if (lenS % l == 0) {
+        if (sigma[threadRank] == c[0] || sigma[threadRank] == c[1] || sigma[threadRank] == c[2]) {
+            S += sigma[threadRank];
+        }
+      }
     S += sigma[threadRank];
   }
 
@@ -293,6 +467,7 @@ void enforceRules(long threadRank) {
         S += sigma[threadRank];
       }
     }
+    
 
     // If we need some c1, add it to the string
     else if (sigma[threadRank] == c[1]) {
