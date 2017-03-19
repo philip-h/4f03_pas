ddc5a41afab9f7e5ff75197f3834df0ecac549cb
= <theo.stone@outlook.com>
2017-03-19 19:47:58 -0400

    Added logic to append server methods

--- pa3/rpc/append_server.c
+++ pa3/rpc/append_server.c
@@ -6,15 +6,30 @@
 
 #include "append.h"
 
+int f;
+int l;
+int m;
+char c0;
+char c1;
+char c2;
+char * host_verify;
+
+char build_str[0] = "";
+
 int *
 rpcinitappendserver_1_svc(append_init_params *argp, struct svc_req *rqstp)
 {
 	static int  result;
 
-	/*
-	 * insert server code here
-	 */
-    result = 5;
+	f = argp->f;
+	l = argp->l;
+	m = argp->m;
+	c0 = argp->c0;
+	c1 = argp->c1;
+	c2 = argp->c2;
+	host_verify = argp->host_verify;
+
+	result = 1;
 
 	return &result;
 }
@@ -24,10 +39,16 @@ rpcappend_1_svc(char *argp, struct svc_req *rqstp)
 {
 	static int  result;
 
-	/*
-	 * insert server code here
-	 */
-    result = 5;
+	result = -1;
+
+	int length = strlen(build_str);
+
+	if(length < m*l)
+	{
+		build_str[length] = *argp;
+		build_str[length+1] = '\0';
+		result = 0;
+	}
 
 	return &result;
 }
