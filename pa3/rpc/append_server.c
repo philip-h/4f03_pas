6abe9ce5d8a9ba20611c7a50d45d3c8e5cbffa5c
Philip Habib <philip.habib6347@gmail.com>
2017-03-20 08:56:10 -0400

    Get enforcement working

--- pa3/rpc/append_server.c
+++ pa3/rpc/append_server.c
@@ -1,6 +1,7 @@
 #include "append.h"
 
 int f;
+int n;
 int l;
 int m;
 char c0;
@@ -10,12 +11,12 @@ char *host_verify;
 
 char *build_str;
 
-int *
-rpcinitappendserver_1_svc(append_init_params *argp, struct svc_req *rqstp)
+int *rpcinitappendserver_1_svc(append_init_params *argp, struct svc_req *rqstp)
 {
 	static int  result;
 
 	f = argp->f;
+    n = argp->n;
 	l = argp->l;
 	m = argp->m;
 	c0 = argp->c0;
@@ -31,22 +32,386 @@ rpcinitappendserver_1_svc(append_init_params *argp, struct svc_req *rqstp)
 	return &result;
 }
 
-int *
-rpcappend_1_svc(char *argp, struct svc_req *rqstp)
+int *rpcappend_1_svc(char *argp, struct svc_req *rqstp)
 {
 	static int  result;
 
 	result = -1;
 
-	int length = strlen(build_str);
+    int length = strlen(build_str);
 
 	if(length < m*l)
 	{
-        build_str[length] = *argp;
-        build_str[length+1] = '\0';
+        switch(f) {
+            case 0: enforce0(*argp); break;
+            case 1: enforce1(*argp); break;
+            case 2: enforce2(*argp); break;
+            case 3: enforce3(*argp); break;
+        }
 		result = 0;
 	}
+    printf("%s\n", build_str);
 
 	return &result;
 }
 
+void append(char c)
+{
+    int len = strlen(build_str);
+    build_str[len] = c;
+    build_str[len+1] = '\0';
+}
+
+void enforce0(char c)
+{
+    int numC0 = 0, numC1 = 0, numC2 = 0;
+
+    int len = strlen(build_str);
+    int i_start = 0, i_end = 0;
+
+    // For enforcing, to check the number of letters, every thread must count the
+    // number of letters in the segment currently being written to... This for
+    // loop calculates this range!
+    for (size_t i = 0; i <= m; i++) {
+        if (len < l*i) {
+            i_start = l*i - l;
+            i_end = l * i;
+            break;
+        }
+    }
+
+    // Count the number of occurrences of c[0..2] in the current segment
+    for (size_t i = i_start; i < i_end; i++) {
+        if     (build_str[i] == c0)  numC0++;
+        else if(build_str[i] == c1)  numC1++;
+        else if(build_str[i] == c2)  numC2++;
+    }
+
+    int numC0Needed = (numC2 - numC1) - numC0;
+    int numC1Needed = (numC2 - numC0) - numC1;
+    int numC2Needed = (numC0 + numC1) - numC2;
+
+    if (n == 3 && l % 2 == 1) {
+        /*ERROR*/
+        fprintf(stderr, "N and L don't work for enforcing rule 0\n");
+        exit(-1);
+    }
+
+
+    int lenCurrSegment = len % l;
+
+    // If the current segment is empty
+    if (lenCurrSegment == 0) {
+        // Only let in a letter used in the property check
+        if (c == c0 || c == c1 || c == c2) {
+            append(c);
+        }
+
+    // If the current segment is not empty and the property is satisfied
+    } else if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+        // If the current segment does not have enough room left
+        if (l - lenCurrSegment == 1) {
+            // Only allow a letter that is not part of the property
+            if (c != c0 && c != c1 && c != c2) {
+                append(c);
+            }
+        // Otherwise, add a letter!!
+        } else {
+            append(c);
+        }
+    // If the current segment is not empty and the current property is not satisfied
+    } else {
+        // Add a letter according to its need
+        if (c == c0 && numC0Needed > 0) {
+            append(c);
+        } else if (c == c1 && numC1Needed > 0) {
+            append(c);
+        } else if (c == c2 && numC2Needed > 0) {
+            append(c);
+        }
+    }
+}
+
+void enforce1(char c)
+{
+    int numC0 = 0, numC1 = 0, numC2 = 0;
+
+    int len = strlen(build_str);
+    int i_start = 0, i_end = 0;
+
+    // For enforcing, to check the number of letters, every thread must count the
+    // number of letters in the segment currently being written to... This for
+    // loop calculates this range!
+    for (size_t i = 0; i <= m; i++) {
+        if (len < l*i) {
+            i_start = l*i - l;
+            i_end = l * i;
+            break;
+        }
+    }
+
+    // Count the number of occurrences of c[0..2] in the current segment
+    for (size_t i = i_start; i < i_end; i++) {
+        if(build_str[i] == c0)  numC0++;
+        else if(build_str[i] == c1)  numC1++;
+        else if(build_str[i] == c2)  numC2++;
+    }
+
+    int numC0Needed = (numC2 - 2*numC1) - numC0;
+    int numC1Needed = (numC2 - numC0)/2 - numC1;
+    int numC2Needed = (numC0 + 2*numC1) - numC2;
+
+    if (n == 3 && l == 1) {
+        /*ERROR*/
+        fprintf(stderr, "N and L don't work for enforcing rule 1\n");
+        exit(-1);
+    }
+
+
+    int lenCurrSegment = len % l;
+
+    // When l == 2 or 4, the solution will never contain c1
+    if (l == 2 || l == 4) {
+        if (c == c1) {
+            /* Don't add a c1*/
+            c = '\0';
+        }
+    }
+    // When l == 3 the solution will never contain c0
+    else if (l == 3) {
+        if (c == c0) {
+            /* Don't add a c0 */
+            c = '\0';
+        }
+        // When l == 3, this calculation should round up
+        numC1Needed = (numC2-numC0+1)/2 -numC1;
+    }
+
+    // If the current segment is empty
+    if (lenCurrSegment == 0) {
+        // Only let in a letter used in the property check
+        if (c == c0 || c == c1 || c == c2) {
+            append(c);
+        }
+
+    // If the current segment is not empty and the property is satisfied
+    } else if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+        // If the incomming letter is a property checked letter
+        if (c == c0 || c == c2 || c == c1) {
+            // If the current segment does not have enough room left
+            if(l - lenCurrSegment == 1 && n == 3){
+                // Reset the segment and try again
+                build_str[len - lenCurrSegment] = '\0';
+            }
+            /* 
+             * If the incomming letter is c0 or c2, make sure 
+             * there are at least 2 spots left. If there are, 
+             * append the letter
+             */
+            else if (c == c0 || c == c2) {
+                if (l - lenCurrSegment >= 2) {
+                    append(c);
+                }
+            /* 
+             * If the incomming letter is c1, make sure 
+             * there are at least 3 spots left. If there are, 
+             * append the letter
+             */
+            } else if(c == c1){
+                if (l - lenCurrSegment >= 3) {
+                    append(c);
+                }
+            }
+        // If the incomming letter is not a property checked letter, add it    
+        } else {
+            append(c);
+        }
+        
+    // If the current segment is not empty and the current property is not satisfied
+    } else {
+        // Add a letter according to it's need
+        if (c == c0 && numC0Needed > 0) {
+            append(c);
+        } else if (c == c1 && numC1Needed > 0) {
+            append(c);
+        } else if (c == c2 && numC2Needed > 0) {
+            append(c);
+        }
+    }
+}
+
+void enforce2(char c)
+{
+    int numC0 = 0, numC1 = 0, numC2 = 0;
+
+    int len = strlen(build_str);
+    int i_start = 0, i_end = 0;
+
+    // For enforcing, to check the number of letters, every thread must count the
+    // number of letters in the segment currently being written to... This for
+    // loop calculates this range!
+    for (size_t i = 0; i <= m; i++) {
+        if (len < l*i) {
+            i_start = l*i - l;
+            i_end = l * i;
+            break;
+        }
+    }
+
+    // Count the number of occurrences of c[0..2] in the current segment
+    for (size_t i = i_start; i < i_end; i++) {
+        if     (build_str[i] == c0)  numC0++;
+        else if(build_str[i] == c1)  numC1++;
+        else if(build_str[i] == c2)  numC2++;
+    }
+
+    int numC0Needed = 1;
+    int numC1Needed = 1;
+    int numC2Needed = (numC0*numC1) - numC2;
+
+
+    // Make sure we are not dividing by 0
+    if (numC1 != 0) {
+        numC0Needed = (numC2/numC1) - numC0;
+    }
+    if (numC0 != 0) {
+        numC1Needed = (numC2/numC0) - numC1;
+    }
+
+    // When l == 1 the solution will never contain c2
+    if (l == 1) {
+        if (c == c2) {
+            /* Don't add a c2*/
+            c = '\0';
+        }
+    }
+
+    int lenCurrSegment = len % l;
+
+    // If the current segment is empty
+    if (lenCurrSegment == 0) {
+        // Only let in a letter used in the property check
+        if (c == c0 || c == c1 || c == c2) {
+            append(c);
+        }
+
+    // If the current segment is not empty and the property is satisfied
+    } else if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+        // If the incomming letter is a property checked letter
+        if (c == c0 || c == c1 || c == c2) {
+            // If the current segment does not have enough room left
+            if (l - lenCurrSegment == 1 && n == 3) {
+                // Reset the segment and try again
+                build_str[len - lenCurrSegment] = '\0';
+            }
+            /* 
+             * If the incomming letter is c0, make sure 
+             * there are at least c1 spots left. If there are, 
+             * append the letter
+             */
+            if (c == c0) {
+                if (l - lenCurrSegment > numC1){
+                    append(c);
+                }
+            }
+            /* 
+             * If the incomming letter is c1, make sure 
+             * there are at least c0 spots left. If there are, 
+             * append the letter
+             */
+            else if (c == c1) {
+                if (l - lenCurrSegment > numC0) {
+                    append(c);
+                }
+            }
+            // If the incomming letter is c2
+            else if (c == c2) {
+                /*Do NOTHING*/
+            }
+        }
+        // If the incomming letter is not a property checked letter, add it    
+        else {
+            append(c);
+        }
+    // If the current segment is not empty and the current property is not satisfied
+    } else {
+        // Add a letter according to it's need
+        if (c == c0 && numC0Needed > 0) {
+            append(c);
+        } else if (c == c1 && numC1Needed > 0) {
+            append(c);
+        } else if (c == c2 && numC2Needed > 0) {
+            append(c);
+        }
+    }
+}
+
+void enforce3(char c)
+{
+    int numC0 = 0, numC1 = 0, numC2 = 0;
+
+    int len = strlen(build_str);
+    int i_start = 0, i_end = 0;
+
+    // For enforcing, to check the number of letters, every thread must count the
+    // number of letters in the segment currently being written to... This for
+    // loop calculates this range!
+    for (size_t i = 0; i <= m; i++) {
+        if (len < l*i) {
+            i_start = l*i - l;
+            i_end = l * i;
+            break;
+        }
+    }
+
+    // Count the number of occurrences of c[0..2] in the current segment
+    for (size_t i = i_start; i < i_end; i++) {
+        if     (build_str[i] == c0)  numC0++;
+        else if(build_str[i] == c1)  numC1++;
+        else if(build_str[i] == c2)  numC2++;
+    }
+
+    int numC0Needed = (numC2 + numC1) - numC0;
+    int numC1Needed = (numC0 - numC2) - numC1;
+    int numC2Needed = (numC0 - numC1) - numC2;
+
+    if (n == 3 && l % 2 == 1) {
+        /*ERROR*/
+        fprintf(stderr, "N and L don't work for enforcing rule 3\n");
+        exit(-1);
+    }
+
+
+    int lenCurrSegment = len % l;
+
+    // If the current segment is empty
+    if (lenCurrSegment == 0) {
+        // Only let in a letter used in the property check
+        if (c == c0 || c == c1 || c == c2) {
+            append(c);
+        }
+
+    // If the current segment is not empty and the property is satisfied
+    } else if (numC0Needed == 0 && numC1Needed == 0 && numC2Needed == 0) {
+        // If the current segment does not have enough room left
+        if (l - lenCurrSegment == 1) {
+            // Only allow a letter that is not part of the property
+            if (c != c0 && c != c1 && c != c2) {
+                append(c);
+            }
+        // Otherwise, add a letter!!
+        } else {
+            append(c);
+        }
+    // If the current segment is not empty and the current property is not satisfied
+    } else {
+        // Add a letter according to its need
+        if (c == c0 && numC0Needed > 0) {
+            append(c);
+        } else if (c == c1 && numC1Needed > 0) {
+            append(c);
+        } else if (c == c2 && numC2Needed > 0) {
+            append(c);
+        }
+    }
+}
--- pa3/rpc/enforce.h
+++ pa3/rpc/enforce.h
@@ -2,3 +2,4 @@ void enforce0(char);
 void enforce1(char);
 void enforce2(char);
 void enforce3(char);
+void append(char);
