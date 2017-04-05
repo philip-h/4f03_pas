e7eeccdb01d2c44e2e0e03b3814c77822e58896e
= <theo.stone@outlook.com>
2017-04-05 16:56:49 -0400

    Fixed pixel processing algorithm

--- pa4/pa4.c
+++ pa4/pa4.c
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
 
   // Finalize the MPI environment.
   // MPI_Finalize();
-  
+
   const int RED = 0;
   const int GREEN = 1;
   const int BLUE = 2;
@@ -24,35 +24,35 @@ int main(int argc, char** argv) {
   int radius = 1;
 
   Image* imgIn = ImageRead("fox.ppm");
-  int width = ImageWidth(imgIn);   
-  int height = ImageHeight(imgIn);   
-
-  printf("WIDTH %d\n", width);
+  int width = ImageWidth(imgIn);
+  int height = ImageHeight(imgIn);
 
   Image* imgOut = ImageCreate(width, height);
 
-  int c = 0;
-  
-  for(int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
+  for(int y = 0; y < height; y++)
+  {
+    for(int x = 0; x < width; x++)
+    {
       int totalR = 0, totalG = 0, totalB = 0, numPixels = 0;
 
-      for (int i = y-radius; i <= y+radius; i++) {
-        for (int j = x-radius; j <= x+radius; j++) {
-
-          if (i < 0 || j < 0 || i >= width || j >= height) {
+      for(int bY = y-radius; bY <= y+radius; bY++)
+      {
+        for(int bX = x-radius; bX <= x + radius;  bX++)
+        {
+          if(bY < 0 || bX < 0 || bX > width || bY > height)
+          {
             continue;
-          } else {
-            totalR += ImageGetPixel(imgIn, j, i, RED);
-            totalG += ImageGetPixel(imgIn, j, i, GREEN);
-            totalB += ImageGetPixel(imgIn, j, i, BLUE);
+          }
+          else
+          {
+            totalR += ImageGetPixel(imgIn, bX, bY, RED);
+            totalG += ImageGetPixel(imgIn, bX, bY, GREEN);
+            totalB += ImageGetPixel(imgIn, bX, bY, BLUE);
             numPixels ++;
           }
         }
       }
 
-      c ++;
-      printf("%d\t num pixels %d\n", c, numPixels);
       int newR = (int)(totalR / numPixels);
       int newG = (int)(totalG / numPixels);
       int newB = (int)(totalB / numPixels);
@@ -62,9 +62,9 @@ int main(int argc, char** argv) {
       ImageSetPixel(imgOut, x, y, BLUE, newB);
 
     }
-  }  
+  }
 
   ImageWrite(imgOut, "out.ppm");
-  
+
   return 0;
 }
